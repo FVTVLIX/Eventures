5f0b304620283f6f0e56593d9e0ccf4ec9f841e7
FVTVLIX <andman290@gmail.com>
2020-06-15 22:32:28 -0400

    added media queries added footer

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Footer.css'
 
 export default function Footer() {
   return (
     <div>
-      
+      <p className="footer">Copyright © 2020 Andres Gonzalez</p>
     </div>
   )
 }
